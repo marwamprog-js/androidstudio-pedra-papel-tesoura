20d375d491a6802bbd1af44ce4d489ef51404e01
Marwam <marwamprog.js@gmail.com>
2023-03-18 10:26:58 -0300

    Sesão 10: Aprenda orientação a objetos com java -Aula 76 -> Escolhendo opção #App3 Pedra Papel ou tesoura - (CONCLUIDO)

--- app/src/main/java/com/cursoandroid/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/cursoandroid/pedrapapeloutesoura/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +29,45 @@ public class MainActivity extends AppCompatActivity {
         this.opcaoSelecionada("tesoura");
     }
 
-    public void opcaoSelecionada(String opcaoSelecionada) {
+    public void opcaoSelecionada(String escolhaUsuario) {
+
+        ImageView imgResultado = findViewById(R.id.imgResultado);
+        TextView textoResultado = findViewById(R.id.textResultado);
+
+
+        int numero = new Random().nextInt(3);
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        String escolhaApp = opcoes[numero];
+
+        switch (escolhaApp) {
+            case "pedra":
+                imgResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imgResultado.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imgResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        // Caso em que o APP seja o GANHADOR
+        if(
+                escolhaApp.equals("pedra") && escolhaUsuario.equals("tesoura") ||
+                escolhaApp.equals("papel") && escolhaUsuario.equals("pedra") ||
+                escolhaApp.equals("tesoura") && escolhaUsuario.equals("papel")
+        ) {
+            textoResultado.setText("Você PERDEU :(");
+        } else if( //Caso em que o Usuário seja o GANHADOR
+                escolhaUsuario.equals("pedra") && escolhaApp.equals("tesoura") ||
+                escolhaUsuario.equals("papel") && escolhaApp.equals("pedra") ||
+                escolhaUsuario.equals("tesoura") && escolhaApp.equals("papel")
+        ) {
+            textoResultado.setText("Você GANHOU :)");
+        } else { // Caso der empate
+            textoResultado.setText("EMPATAMOS :)");
+        }
+
 
     }
 }
